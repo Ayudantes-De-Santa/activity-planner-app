e5fa052cd8091080360f4b31c8188b592cde56cf
Agustin Lacomba <alacomba@velocitypartners.net>
2018-09-03 14:07:48 -0300

    fixes

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { login } from '../../actions/activity'
+import '../../styles/login.css';
+
 
 class Login extends React.Component {
 
@@ -18,13 +20,17 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <form>
+            <div class="login-page">
+
+              <div class="form">  
+              <img class="endava-image" src="https://www.endava.com/-/media/EndavaDigital/Endava/Images/MetaDataImages/preview-image.ashx"/>
+              <form>   
                     Enter your name:
                     <input type="text" name="name" onChange={this.handleChange}></input>
                 </form>
                 <input type="submit" onClick={() => this.props.loginProp(this.state.name)}  value="Submit"></input>
             </div>
+        </div>
         ) // 
     }
 }
