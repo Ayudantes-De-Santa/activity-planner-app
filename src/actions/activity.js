bf897bab5d1400659396ada5f847109894b2c0a7
Agustin Lacomba <alacomba@velocitypartners.net>
2018-09-03 16:10:08 -0300

    fix activity form

--- src/actions/activity.js
+++ src/actions/activity.js
@@ -13,6 +13,20 @@ export const login = (name) => (
         name
     }
 )
+export const createActivity = (activity) => async dispatch => {
+     console.log(activity)
+    try {
+       const response = await isoFetch('/activity/create', postWithJSONBody(activity))
+      if(response.status===200){
+        dispatch(fetchActivities())
+      }
+      else{
+        console.log("Error!! ", response.status)
+      }
+    } catch (err) {
+      console.log("Error!! ", err)
+    }
+}
 
 export const fetchActivities = () => async dispatch => {
     console.log("fetching activities")
--- src/components/activity-form/activity-form.jsx
+++ src/components/activity-form/activity-form.jsx
@@ -1,29 +1,73 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { sayHello } from '../../actions/activity'
+import { createActivity } from '../../actions/activity'
 
 
-let ActivityForm = ({}) => (
-<div>
-<h2>Create activity</h2>
-<br/>
-		<form action="" method="POST" class="form" role="form">
-            <input class="form-control" name="name" placeholder="Name" type="text" />
-            <textarea name="description" id="description" class="form-control" rows="9" cols="25" required="required" placeholder="Description"></textarea>
-            <input class="form-control" name="date" placeholder="Date" type="text" />
-            <input class="form-control" name="location" placeholder="Location" type="text" />
-     <br/>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">
-               Add activity</button>
-            </form>
+class ActivityForm extends React.Component {
+
+	constructor(props){
+		super(props)
+		this.state = {
+
+		}
+	}
+
+    handleNameChange = event => {
+        this.setState({ name: event.target.value })
+    }
+     handleDescriptionChange = event => {
+        this.setState({ description: event.target.value })
+    }
+     handleLocationChange = event => {
+        this.setState({ location: event.target.value })
+    }
+     handleDateChange = event => {
+        this.setState({ date: event.target.value })
+    }
+    
+    getPostableJson =()=>{
+    	let state = {
+   			 name : this.state.name,
+   			 description : this.state.description,
+   			 location : this.state.location,
+   			 date: this.state.date}
+
+		return JSON.stringify(state);
+    }
+
+	render() {
+		const createActivity = this.props.createActivity
+		return (
+			<div>
+				<h2>Create activity</h2>
+				<br/>
+				<form action="#" method="POST" class="form" role="form">
+		            <input class="form-control" name="name" placeholder="Name" type="text" onChange={this.handleNameChange} />  
+   					<textarea name="description" id="description" class="form-control" rows="9" cols="25" required="required" placeholder="Description" onChange={this.handleDescriptionChange}></textarea>
+		            <input class="form-control" name="date" placeholder="Date" type="text" onChange={this.handleDateChange}/>
+		            <input class="form-control" name="location" placeholder="Location" type="text" onChange={this.handleLocationChange}/>
+		            <button class="btn btn-lg btn-primary btn-block" type="button" onClick={()=>createActivity(this.getPostableJson())}>
+		               Add activity</button>
+	            </form>
             </div>
-)
+		)
+	}
+}
 
+/*
+   <textarea name="description" id="description" class="form-control" rows="9" cols="25" required="required" placeholder="Description"></textarea>
+		            <input class="form-control" name="date" placeholder="Date" type="text" />
+		            <input class="form-control" name="location" placeholder="Location" type="text" />
+				     <br/>
+				     -->
+		         
+
+*/
 const mapStateToProps = (state) => ({
   
 })
 const mapDispatchToProps = (dispatch) => ({
-//   saySomething: () => { dispatch(sayHello())}
+	createActivity: (activity) => { dispatch(createActivity(activity))}
 })
 ActivityForm = connect(
   mapStateToProps,
